688b886bcd5aaf0c48e43ee1df7a277d7515d566
Cyrus245 <csakib78@gmai.com>
2022-07-21 16:53:42 +0600

    first commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.use(bodyparser.urlencoded({
     extended: true
 }));
-
+app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res) => {
@@ -36,18 +36,34 @@ app.post("/", (req, res) => {
 
             const sName = response.data[0].show.name;
             const genre = response.data[0].show.genres;
-            const image = response.data[0].show.image.medium;
+            const image = response.data[0].show.image.original;
             const rating = response.data[0].show.rating.average;
+            const summary = response.data[0].show.summary;
 
 
-            res.write(`<h1>Show Name:-${sName}</h1>`);
-            res.write(`<h2>Genre:-${genre}</h2>`);
-            res.write(`<p>Rating:${rating}</p>`);
-            res.write(`<img src ="${image}">`);
-            res.send();
+            // res.write(`<h1>Show Name:-${sName}</h1>`);
+            // res.write(`<h2>Genre:-${genre}</h2>`);
+            // res.write(`<p>Rating:${rating}</p>`);
+            // res.write(`<img src ="${image}">`);
+            // res.send();
+
+            options = {
+
+                showName: sName,
+                genre: genre,
+                image: image,
+                rating: rating,
+                summary: summary,
+
+            }
+
+
+            res.render('response', options)
 
 
         })
+
+
         .catch(e => {
 
             console.log(`error ${e}`);
@@ -55,6 +71,13 @@ app.post("/", (req, res) => {
 
 
 
+
+})
+
+
+app.post("/response", (req, res) => {
+
+    res.redirect("/")
 
 })
 
